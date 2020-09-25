0adf9feda4688030ad37901852a51bfbf63431bc
Alexey Tarabanov <tarabanov@bk.ru>
2020-09-25 12:19:27 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,48 @@
 package day5;
 
+/**
+ * 1. Создать класс Автомобиль (англ. Car), с полями “Год выпуска”, “Цвет”, “Модель”.
+ * Создать геттеры и сеттеры для каждого поля.
+ * Создать экземпляр класса Автомобиль, задать сеттером каждое поле,
+ * вывести в консоль значение каждого поля геттером.
+ * Созданный вами класс должен отвечать принципам инкапсуляции.*/
+
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setModel("Toyota");
+        car.setColor("Black");
+        car.setManufactureYear(2020);
+        System.out.println(car.getModel() + "\n" + car.getColor() + "\n" + car.getManufactureYear());
+    }
+}
 
+class Car {
+    private int manufactureYear;
+    private String color;
+    private String model;
+
+    public int getManufactureYear() {
+        return manufactureYear;
+    }
+
+    public void setManufactureYear(int manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,7 +1,40 @@
 package day5;
 
+/**
+ * 2. Создать класс Мотоцикл (англ. Motorbike), с  полями “Год выпуска”, “Цвет”, “Модель”.
+ * Создать экземпляр класса Мотоцикл, с помощью конструктора (сеттеры не использовать).
+ * Придерживаться принципов инкапсуляции и использовать ключевое слово this.
+ * Геттером получить год выпуска, цвет, модель, вывести значения в консоль.*/
+
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike(2000, "Grey", "Harley-Davidson");
+        System.out.println(motorbike.getManufactureYear());
+        System.out.println(motorbike.getColor());
+        System.out.println(motorbike.getModel());
+    }
+}
 
+class Motorbike {
+    private int manufactureYear;
+    private String color;
+    private String model;
+
+    public Motorbike(int manufactureYear, String color, String model) {
+        this.manufactureYear = manufactureYear;
+        this.color = color;
+        this.model = model;
+    }
+
+    public int getManufactureYear() {
+        return manufactureYear;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
     }
 }
