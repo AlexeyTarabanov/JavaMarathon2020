b8a49fb3f9d6e7b81529a545da9eb948092d8c4b
Alexey Tarabanov <tarabanov@bk.ru>
2020-10-06 00:15:07 +0300

    complete day14 tasks

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,49 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * 1. Создать .txt файл в папке проекта, как показано в видео (урок 36, время 15:30).
+ * Заполнить его вручную десятью произвольными числами.
+ * Реализовать программу, которая считает сумму всех чисел в этом файле и выводит ее на экран.
+ * Если файла не существует в папке проекта, в программе необходимо выбрасывать исключение и выводить в консоль сообщение
+ * “Файл не найден”.
+ * Помимо этого, если чисел в файле меньше или больше 10, необходимо выбрасывать исключение и выводить в консоль сообщение
+ * “Некорректный входной файл”.
+ */
+
 public class Task1 {
     public static void main(String[] args) {
 
+        File file = new File("test.txt");
+        printSumDigits(file);
+    }
+
+    public static void printSumDigits(File file) {
+
+        int sum = 0;
+
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] numbers = line.split(" ");
+            for (int i = 0; i < numbers.length; i++) {
+                sum += Integer.parseInt(numbers[i]);
+            }
+
+            if (numbers.length == 10) {
+                System.out.println(sum);
+            } else {
+                throw new NumberFormatException();
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (NumberFormatException e) {
+            System.out.println("Некорректный входной файл");
+        }
     }
 }
--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,7 +1,59 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 2. Создать в папке проекта файл people.txt в котором хранятся имена и возраст людей.
+ * Реализовать статический метод List<String> parseFileToStringList(),
+ * который считывает содержимое этого файла и возвращает список, состоящий из значений имен и возрастов каждого человека.
+ * Получить данный список в методе main() и распечатать его в консоль.
+ * В случае, если файла не существует в папке проекта, в программе необходимо выбрасывать исключение и выводить
+ * в консоль сообщение “Файл не найден”. Помимо этого, если значение возраста отрицательно, необходимо выбрасывать
+ * исключение и выводить в консоль сообщение “Некорректный входной файл”.
+ *
+ * Пример ответа: [Mike 24, John 19, Anna 20, Miguel 5]
+ *
+ * Пример содержания файла people.txt:
+ * Mike 24
+ * John 19
+ * Anna 20
+ * Miguel 5*/
+
 public class Task2 {
     public static void main(String[] args) {
 
+        File file = new File("people");
+        System.out.println(parseFileToStringList(file));
+    }
+
+    public static List<String> parseFileToStringList(File file) {
+
+        List<String> list = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] numbers = line.split(" ");
+                int age = Integer.parseInt(numbers[1]);
+                if (age > 0) {
+                    list.add(line);
+                } else {
+                    throw new IOException();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IOException e) {
+            System.out.println("Некорректный входной файл");
+        }
+
+        return list;
     }
 }
