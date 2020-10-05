d403970170e6da2caf9c8a07f1c3896b43eba45c
Alexey Tarabanov <tarabanov@bk.ru>
2020-10-05 14:41:42 +0300

    complete day13 tasks

---
+++ src/main/java/day13/User.java
@@ -0,0 +1,52 @@
+package day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String username;
+    private List<User> subscriptions;
+
+    public User(String username) {
+        this.username = username;
+        this.subscriptions = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<User> getSubscriptions() {
+        return subscriptions;
+    }
+
+    public void subscribe(User user) {
+        subscriptions.add(user);
+    }
+
+    public boolean isSubscribed(User user) {
+        for(User u : subscriptions) {
+            if (u.getUsername().equals(user.getUsername())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean isFriend(User user) {
+        if (user.isSubscribed(this) && this.isSubscribed(user)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void sendMessage(User user, String text) {
+        MessageDatabase.sendMessage(this, user, text);
+    }
+
+    @Override
+    public String toString() {
+        return username;
+    }
+}
